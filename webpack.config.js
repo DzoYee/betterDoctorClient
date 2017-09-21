f53b0dddef51fb416815e7d66197ab18dce2e922
Joey Leung <joey.leung@farmersinsurance.com>
2017-09-20 22:52:54 -0700

    Added src public dirs

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 const webpack = require('webpack');
 
 let config = {
-  entry:'./index.js',
+  entry:'./src/index.js',
   output: {
-    path: path.join(__dirname, 'www'),
+    path: path.join(__dirname, './public'),
     filename: 'bundle.js'
   },
   module: {
