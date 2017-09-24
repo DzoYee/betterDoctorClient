5f1d5f0d6d735f475499a80b15fbb0ad603856b8
Joey Leung <joey.leung@farmersinsurance.com>
2017-09-23 18:08:12 -0700

    Pass Service from App to Searchbar

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,11 +3,16 @@ import SearchBar from '../components/search_bar.js';
 import DoctorList from '../components/doctor_list.js';
 
 class App extends Component {
+  doctorSearch(term) {
+    console.log('turkey');
+  }
+
   render() {
+    // const doctorSearch = this.doctorSearch;
     return (
       <div>
         <h1>Hello from my React component!</h1>
-        <SearchBar/>
+        <SearchBar onSearchTermChange={this.doctorSearch} />
         <DoctorList />
       </div>
     );
