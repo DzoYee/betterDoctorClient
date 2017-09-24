56debf63f408d0351ae0b6bc2c2fac1a41c60144
Joey Leung <joey.leung@farmersinsurance.com>
2017-09-23 18:50:35 -0700

    First Connection to Backend

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import SearchBar from '../components/search_bar.js';
 import DoctorList from '../components/doctor_list.js';
+import _ from 'underscore';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      suggestions: [],
+    };
+  }
+
   doctorSearch(term) {
-    console.log('turkey');
+    fetch('http://localhost:8000/autocomplete/' + term)
+    .then(results => {
+      console.log("updated: ", results);
+      this.setState({
+        suggestions: suggestions
+      });
+    });
   }
 
   render() {
-    // const doctorSearch = this.doctorSearch;
+    const doctorSearch = _.this.doctorSearch;
     return (
       <div>
         <h1>Hello from my React component!</h1>
-        <SearchBar onSearchTermChange={this.doctorSearch} />
+        <SearchBar 
+          onSearchTermChange={this.doctorSearch}
+          suggestions={this.state.suggestions} />
         <DoctorList />
       </div>
     );
--- src/components/search_bar_suggestions.js
+++ src/components/search_bar_suggestions.js
@@ -2,17 +2,19 @@ import React from 'react';
 import SearchBarSuggestion from './search_bar_suggestion.js';
 
 const SearchBarSuggestions = (props) => {
-  // const suggestions = props.suggestions.map((suggestion) => {
-  //   return (
-  //     <SearchBarSuggestion />
-  //   );
-  // });
+  console.log(props);
+  let suggestions = "test";
+  if (props.suggestions) {
+    const suggestions = props.suggestions.map((suggestion) => {
+      return (
+        <SearchBarSuggestion />
+      );
+    });
+  }
   return (
-    <div>
-      <ul>
-        
-      </ul>
-    </div>
+    <ul>
+      {suggestions}
+    </ul>
   );
 };
 
