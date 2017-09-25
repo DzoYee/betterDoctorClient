54f6ef7bcd848ad3aa0034b101c4abe6c7ae4d8b
Joey Leung <joey.leung@farmersinsurance.com>
2017-09-24 17:07:03 -0700

    Add searchBar test

---
+++ __tests__/search_bar.test.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { mount } from 'enzyme';
+import SearchBar from '../src/components/search_bar.js';
+import SearchBarSuggestions from '../src/components/search_bar_suggestions.js';
+
+describe('SearchBar', () => {
+  let props;
+  let mountedSearchBar;
+  const searchBar = () => {
+    if (!mountedSearchBar) {
+      mountedSearchBar = mount(
+        <SearchBar {...props} />
+      );
+    }
+    return mountedSearchBar;
+  }
+
+  beforeEach(() => {
+    props = {
+      onSearchTermChange: undefined,
+      doctorSearch: undefined,
+      suggestions: []
+    };
+    mountedSearchBar = undefined;
+  });
+
+  it("always renders a div", () => {
+    const divs = searchBar().find('div');
+    expect(divs.length).toBeGreaterThan(0);
+  });
+
+  describe("the rendered div", () => {
+    it("contains everything else that gets rendered", () => {
+      const divs = searchBar().find("div");
+      
+      const wrappingDiv = divs.first();
+      expect(wrappingDiv.children()).toEqual(searchBar().children());
+    });
+  });
+
+  it("always renders a 'SearchBarSuggestions'", () => {
+    expect(searchBar().find(SearchBarSuggestions).length).toBe(1);
+  });
+
+  describe("rendered 'SearchBarSuggestions'", () => {
+    it("does not receive any props", () => {
+      const searchBarSuggestions = searchBar().find(SearchBarSuggestions);
+      expect(Object.keys(searchBarSuggestions.props()).length).toBe(1);
+    });
+  });
+})
